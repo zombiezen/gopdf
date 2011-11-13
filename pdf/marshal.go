b246c494b2c801886af0f45c285d1ed5fc5193ac
Ross Light <ross@zombiezen.com>
2011-11-13 11:06:17 -0800

    Change marshalling to state-based approach

--- pdf/marshal.go
+++ pdf/marshal.go
@@ -14,46 +14,62 @@ type Marshaler interface {
 	MarshalPDF() ([]byte, os.Error)
 }
 
-const marshalFloatPrec = 5
-
 // Marshal returns the PDF encoding of v.
 //
 // If the value implements the Marshaler interface, then its MarshalPDF method
 // is called.  ints, strings, and floats will be marshalled according to the PDF
 // standard.
 func Marshal(v interface{}) ([]byte, os.Error) {
-	return marshalValue(reflect.ValueOf(v))
+	state := new(marshalState)
+	if err := state.marshalValue(reflect.ValueOf(v)); err != nil {
+		return nil, err
+	}
+	return state.Bytes(), nil
 }
 
-const nullKeyword = "null"
+type marshalState struct {
+	bytes.Buffer
+}
 
-func marshalValue(v reflect.Value) ([]byte, os.Error) {
+const marshalFloatPrec = 5
+
+func (state *marshalState) marshalValue(v reflect.Value) os.Error {
 	if !v.IsValid() {
-		return []byte(nullKeyword), nil
+		state.WriteString("null")
+		return nil
 	}
 
 	if m, ok := v.Interface().(Marshaler); ok {
-		return m.MarshalPDF()
+		b, err := m.MarshalPDF()
+		if err != nil {
+			return err
+		}
+		state.Write(b)
+		return nil
 	}
 
 	switch v.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return []byte(strconv.Itoa64(v.Int())), nil
+		state.WriteString(strconv.Itoa64(v.Int()))
+		return nil
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		return []byte(strconv.Uitoa64(v.Uint())), nil
+		state.WriteString(strconv.Uitoa64(v.Uint()))
+		return nil
 	case reflect.Float32, reflect.Float64:
-		return []byte(strconv.Ftoa64(v.Float(), 'f', marshalFloatPrec)), nil
+		state.WriteString(strconv.Ftoa64(v.Float(), 'f', marshalFloatPrec))
+		return nil
 	case reflect.String:
-		return []byte(quote(v.String())), nil
+		state.WriteString(quote(v.String()))
+		return nil
 	case reflect.Ptr:
-		return marshalValue(v.Elem())
+		return state.marshalValue(v.Elem())
 	case reflect.Array, reflect.Slice:
-		return marshalSlice(v)
+		return state.marshalSlice(v)
 	case reflect.Map:
-		return marshalDictionary(v)
+		return state.marshalDictionary(v)
 	}
 
-	return nil, os.NewError("pdf: unsupported type: " + v.Type().String())
+	return os.NewError("pdf: unsupported type: " + v.Type().String())
 }
 
 // quote escapes a string and returns a PDF string literal.
@@ -70,48 +86,39 @@ func quote(s string) string {
 	return "(" + r.Replace(s) + ")"
 }
 
-var arrayMarshalChars = []byte{'[', ' ', ']'}
-
-func marshalSlice(v reflect.Value) ([]byte, os.Error) {
-	marshalled := make([][]byte, 0, v.Len()+2)
-	marshalled = append(marshalled, arrayMarshalChars[0:1])
+func (state *marshalState) marshalSlice(v reflect.Value) os.Error {
+	state.WriteString("[ ")
 	for i := 0; i < v.Len(); i++ {
-		m, err := marshalValue(v.Index(i))
-		if err != nil {
-			return nil, err
+		if err := state.marshalValue(v.Index(i)); err != nil {
+			return err
 		}
-		marshalled = append(marshalled, m)
+		state.WriteByte(' ')
 	}
-	marshalled = append(marshalled, arrayMarshalChars[2:3])
-	return bytes.Join(marshalled, arrayMarshalChars[1:2]), nil
+	state.WriteString("]")
+	return nil
 }
 
-var dictionaryMarshalChars = []byte{'<', '<', ' ', '>', '>'}
-
-func marshalDictionary(v reflect.Value) ([]byte, os.Error) {
+func (state *marshalState) marshalDictionary(v reflect.Value) os.Error {
 	if v.Type().Key() != reflect.TypeOf(name("")) {
-		return nil, os.NewError("pdf: cannot marshal dictionary with key type: " + v.Type().Key().String())
+		return os.NewError("pdf: cannot marshal dictionary with key type: " + v.Type().Key().String())
 	}
 
-	marshalled := make([][]byte, 0, v.Len()*2+2)
-	marshalled = append(marshalled, dictionaryMarshalChars[0:2])
-
+	state.WriteString("<< ")
 	for _, k := range v.MapKeys() {
 		// Marshal key
 		mk, err := k.Interface().(name).MarshalPDF()
 		if err != nil {
-			return nil, err
+			return err
 		}
+		state.Write(mk)
+		state.WriteByte(' ')
 
 		// Marshal value
-		mobj, err := marshalValue(v.MapIndex(k))
-		if err != nil {
-			return nil, err
+		if err := state.marshalValue(v.MapIndex(k)); err != nil {
+			return err
 		}
-
-		marshalled = append(marshalled, mk, mobj)
+		state.WriteByte(' ')
 	}
-
-	marshalled = append(marshalled, dictionaryMarshalChars[3:5])
-	return bytes.Join(marshalled, dictionaryMarshalChars[2:3]), nil
+	state.WriteString(">>")
+	return nil
 }
