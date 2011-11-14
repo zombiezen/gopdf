44e6797d2115212b24e4df6c8daade4a9693b676
Ross Light <ross@zombiezen.com>
2011-11-14 12:16:48 -0800

    Add Path unit test

--- pdf/canvas_test.go
+++ pdf/canvas_test.go
@@ -6,6 +6,22 @@ import (
 	"testing"
 )
 
+const pathExpectedOutput = `12 34 m
+-56 78 l
+h
+`
+
+func TestPath(t *testing.T) {
+	path := new(Path)
+	path.Move(12, 34)
+	path.Line(-56, 78)
+	path.Close()
+
+	if path.buf.String() != pathExpectedOutput {
+		t.Errorf("Output was %q, expected %q", path.buf.String(), pathExpectedOutput)
+	}
+}
+
 const textExpectedOutput = `/Helvetica 12 Tf
 14 TL
 (Hello, World!) Tj
