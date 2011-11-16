8bee8214ce02c8f831171954912acde153e587b5
Ross Light <ross@zombiezen.com>
2011-11-16 11:55:54 -0800

    Move text testing to separate file

---
+++ pdf/text_test.go
@@ -0,0 +1,35 @@
+// Copyright (C) 2011, Ross Light
+
+package pdf
+
+import (
+	"testing"
+)
+
+const textExpectedOutput = `/Helvetica 12 Tf
+14 TL
+(Hello, World!) Tj
+T*
+(This is SPARTA!!1!) Tj
+`
+
+func TestText(t *testing.T) {
+	text := new(Text)
+	text.SetFont(Helvetica, 12)
+	text.SetLeading(14)
+	text.Text("Hello, World!")
+	text.NextLine()
+	text.Text("This is SPARTA!!1!")
+
+	if text.buf.String() != textExpectedOutput {
+		t.Errorf("Output was %q, expected %q", text.buf.String(), textExpectedOutput)
+	}
+
+	if len(text.fonts) == 1 {
+		if !text.fonts[Helvetica] {
+			t.Error("Helvetica missing from fonts")
+		}
+	} else {
+		t.Errorf("Got %d fonts, expected %d", len(text.fonts), 1)
+	}
+}
